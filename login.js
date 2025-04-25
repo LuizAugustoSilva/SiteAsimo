dbe7ca28849554d434fa59719d2c82d2c61ac2b2
Luiz Augusto Silva <luizaugustosilva871@gmail.com>
2025-04-25 16:44:25 -0300

    terminando o prototipo da Home Page e arrumando o page de login

--- login.js
+++ login.js
@@ -6,14 +6,18 @@ const btnSubmit = document.querySelector('.submit');
 
 const toggleSenha = () => {
     event.preventDefault();
-    if(senhaLogin.type === "password") {
-        senhaLogin.type = "text";
-        iconeOlho.src = 'Figuras/olho-aberto.png';
-    }
-    else {
-        senhaLogin.type = "password";
-        iconeOlho.src = 'Figuras/olho-fechado.png';
-    }
+    iconeOlho.style.opacity = 0;
+    setTimeout(() => {
+        if(senhaLogin.type === "password") {
+            senhaLogin.type = "text";
+            iconeOlho.src = 'Figuras/olho-aberto.png';
+        }
+        else {
+            senhaLogin.type = "password";
+            iconeOlho.src = 'Figuras/olho-fechado.png';
+        }
+        iconeOlho.style.opacity = 1
+    }, 200);
     senhaLogin.focus();
 }
 
