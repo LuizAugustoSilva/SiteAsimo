a1b10122708344820d6ad6650af4d0400dfe179d
Luiz Augusto Silva <luizaugustosilva871@gmail.com>
2025-04-26 15:03:04 -0300

    transição de background e ajustando rodape

---
+++ homePage.js
@@ -0,0 +1,42 @@
+//=============================== referencias ============================
+
+const navUlRef = document.querySelector('#navUl');
+const viewPrincipal = document.querySelector('.view-principal');
+
+//=============================== imagens background ==============================
+
+const imagens = ['url(Figuras/fundacao-membros.jpg)', 'url(Figuras/img1.jpg)', 'url(Figuras/img2.jpg)', 'url(Figuras/img3.jpg)'];
+
+//=============================== functions ==============================
+
+const roll = (evt) => {
+    const destinoName = evt.target.dataset.name;
+    const destinoRef = document.querySelector(`#${destinoName}`);
+    
+    if(destinoName === 'login'){
+        window.open('login.html', '_self')
+        return;
+    }
+
+    window.scrollTo({
+        top: destinoRef.getBoundingClientRect().top + window.scrollY - 112,
+        behavior: "smooth"
+    });
+}
+
+let index = 0;
+
+setInterval(() => {
+    viewPrincipal.style.backgroundImage = imagens[index];
+    console.log('oi')
+    index++;
+    if (index >= imagens.length) {
+        index = 0;
+    }
+}, 3000);
+
+//=============================== addEvents ==============================
+
+for (let i = 0; i < navUlRef.childNodes.length; i++) {
+    navUlRef.childNodes[i].addEventListener('click', roll);
+}
